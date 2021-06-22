990f48c5de4e1ff434aafa8c43d0a96a7f72daaa
oilcake <oilpie@gmail.com>
2021-06-22 15:17:42 +0300

    weback works

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const Post = require('postcss-preset-env');
+// const Post = require('postcss-preset-env');
 
 module.exports = {
-    
-    //mode: 'development',
+
+    // mode: 'development',
     entry: './source/js/index.js',
     devtool: 'source-map',
     output: {
@@ -13,23 +13,23 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
     },
 
-        module: {
-            rules: [
-              {
-                test: /\.s[ac]ss/,
-                use: post.extract({
-                fallback: 'style-loader',
-                use: ['css-loader', 'postcss-loader', 'sass-loader'],
-              }),
-            }
-            ],
-          },
+        // module: {
+            // rules: [
+              // {
+                // test: /\.s[ac]ss/,
+                // use: post.extract({
+                // fallback: 'style-loader',
+          //       use: ['css-loader', 'postcss-loader', 'sass-loader'],
+          //     }),
+          //   }
+          //   ],
+          // },
           plugins: [
             new HTMLWebpackPlugin({
               template: './source/index.html',
             }),
-            new CleanWebpackPlugin({
-              filename: filename('css')
-            })
+            new CleanWebpackPlugin(
+              // filename: filename('css')
+            )
            ]
     };

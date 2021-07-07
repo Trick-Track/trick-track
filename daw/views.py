fcd36378e4224af27f79a755b4272516fbafcc24
oilcake <oilpie@gmail.com>
2021-07-08 00:36:49 +0300

    login on main page

--- daw/views.py
+++ daw/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 
 def index(request):
     return render(request, 'daw/index.html')
+
+def login(request):
+    return redirect('accounts/login')
 

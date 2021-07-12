fd45f21d8c724bb3b220b42217e40ffd3f922453
oilcake <oilpie@gmail.com>
2021-07-13 01:34:33 +0300

    WIP

--- daw/models.py
+++ daw/models.py
@@ -5,4 +5,7 @@ from django.contrib.auth.models import User, auth
 class Project(models.Model):
 	name = models.CharField(max_length=20)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-# 	pass
+
+	class Meta:
+		abstract = True
+

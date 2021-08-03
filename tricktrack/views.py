238703a94bb5f7049fdd74eb50e64598f6602c62
oilcake <oilpie@gmail.com>
2021-08-03 19:43:01 +0300

    list of projects

--- tricktrack/views.py
+++ tricktrack/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 
 def index(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        return redirect('/projects')
+    else:
+        return render(request, 'index.html')
 
 def profile(request):
-    print('redirecting from main')
     return redirect('/projects')

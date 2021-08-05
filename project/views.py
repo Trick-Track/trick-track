6429d93c2749ce626b0856db79747db617b99b19
oilcake <oilpie@gmail.com>
2021-08-06 02:42:23 +0300

    working on saving

--- project/views.py
+++ project/views.py
@@ -17,13 +17,16 @@ def serialize(projects):
 def validate(project):
     pass
 
-def save(data, user):
-    name = data['name']
-    bpm = data['bpm']
-    lanes = data['lanes']
-    project = Project(name=name, bpm=bpm, user=user, lanes=lanes)
+def save(from_frontend, user):
+    name = from_frontend['name']
+    # print('name = ', name)
+    bpm = from_frontend['bpm']
+    lanes = from_frontend['lanes']
     if not Project.objects.filter(user=user, name=name).exists():
+        project = Project(name=name, bpm=bpm, user=user, lanes=lanes)
         project.save()
+    else:
+        project = Project.objects.get(user=user, name=name)
     return project
     
 
@@ -48,14 +51,17 @@ def delete_all(user):
 def projects(request):
     if request.user.is_authenticated:
         user = request.user
+        # print(user)
         if request.method == 'GET':
             projects_list = retrieve_all(user)
             serialized_list = serialize(projects_list)
             return JsonResponse(serialized_list, encoder=DjangoJSONEncoder, safe=False)
         elif request.method == 'POST':
             project = json_decode(request)
-            saved = save(project, user)
-            serialized = serialize([saved, ])
+            print(project)
+            saved_project = save(project, user)
+            serialized = serialize([saved_project, ])
+            # print(serialized)
             return JsonResponse(serialized, encoder=DjangoJSONEncoder, safe=False)
         elif request.method == 'DELETE':
             delete_all(user)

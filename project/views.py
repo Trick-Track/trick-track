7f4fbf67443f7103d4b5d8bf3194c6cb09ed2338
oilcake <oilpie@gmail.com>
2021-08-06 23:37:55 +0300

    a bit of PEP8

--- project/views.py
+++ project/views.py
@@ -1,21 +1,23 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import redirect
 from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User, auth
 from django.core import serializers
 from django.core.serializers.json import DjangoJSONEncoder
 from .models import Project
 import json
 
+
 def json_decode(request):
     json_data = json.loads(request.body.decode("utf-8"))
     return json_data
 
+
 def serialize(projects):
     return serializers.serialize('json', projects)
 
+
 def valid(project):
     return project
+
 
 def new(request, user):
     new_project = json_decode(request)
@@ -29,7 +31,8 @@ def new(request, user):
         else:
             project = Project.objects.get(user=user, name=name)
     return project
-    
+
+
 def update(request, id):
     version = json_decode(request)
     bpm = version['bpm']
@@ -40,20 +43,25 @@ def update(request, id):
     project.save()
     return project
 
+
 def retrieve(id):
     project = Project.objects.get(pk=id)
     return project
+
 
 def retrieve_all(user):
     user_projects = Project.objects.filter(user=user)
     return user_projects
 
+
 def delete(id):
     project = Project.objects.get(pk=id)
     project.delete()
 
+
 def delete_all(user):
     Project.objects.filter(user=user).delete()
+
 
 def projects(request):
     if request.user.is_authenticated:
@@ -70,6 +78,7 @@ def projects(request):
             delete_all(user)
     else:
         return redirect('accounts/login/')
+
 
 def project(request, id=id):
     if request.user.is_authenticated:
@@ -94,9 +103,9 @@ def project(request, id=id):
     else:
         return redirect('accounts/login/')
 
-    # function to delete all projects of current user 
-    # in response to url 'projects/delete_everything'
 
+# function to delete all projects of current user 
+# in response to url 'projects/delete_everything'
 def delete_everything(request):
     user = request.user
     delete_all(user)
--- tricktrack/views.py
+++ tricktrack/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from project.views import retrieve_all
-from project.models import Project
+
 
 def index(request):
     if request.user.is_authenticated:
         user = request.user
-        return render(request, 'index.html', {'projects':retrieve_all(user)})
+        return render(request, 'index.html', {'projects': retrieve_all(user)})
     else:
         return render(request, 'index.html')
+
 
 def profile(request):
     return redirect('/')
